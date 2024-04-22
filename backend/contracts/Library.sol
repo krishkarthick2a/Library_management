262f873cb90b675582aae57cde31aad09e7a2f56
unknown <karthick.n@simplyfi.tech>
2024-04-22 19:03:19 +0530

    onlyOwner

--- backend/contracts/Library.sol
+++ backend/contracts/Library.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//signer :  0x6b91318da72D33CaD5f71502101bCD7563068dFe
-// deployed at : 0x612721395Dcf3A14B5535C3617f89929864E76aB
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+//token address :  0xB7e2038753547602893c9a4c7C9D5CCE358b93b3
+// deployed at : 0x6d92Cf4bD6205cFd7C1309D6EBA65A11D8c49ce7
 contract Library {
     struct Book {
         uint256 id;
         string title;
+        uint256 price;
         address borrower;
         bool checkedOut;
     }
@@ -15,6 +17,7 @@ contract Library {
     struct User {
         address id;
         string name;
+        uint256 balance;
         uint256[] borrowedBooks;
     }
 
@@ -27,31 +30,45 @@ contract Library {
     event BookCheckedOut(uint256 indexed bookId, address indexed borrower);
     event BookReturned(uint256 indexed bookId);
     event UserAdded(address indexed userId, string name);
+    address public token;
+    address public owner;
 
-    constructor() {
-        totalBooks = 0;
-        totalUsers = 0;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
     }
+    constructor(address _ERC20_ContractAddress) {
+        owner = msg.sender;
+        token = _ERC20_ContractAddress;
+            totalBooks = 0;
+            totalUsers = 0;
+    }
+    
 
-    function addBook(string memory _title) external {
+    function addBook(string memory _title, uint256 _price) external onlyOwner {
         totalBooks++;
-        books[totalBooks] = Book(totalBooks, _title, address(0), false);
+        books[totalBooks] = Book(totalBooks, _title, _price, address(0), false);
         emit BookAdded(totalBooks, _title);
     }
 
-    function addUser(string memory _name) external {
+    function addUser(string memory _name,address _Id, uint256 _tokens ) external onlyOwner returns(bool){
         totalUsers++;
-        users[msg.sender] = User(msg.sender, _name, new uint256[](0));
+        IERC20(token).transferFrom(owner,_Id, _tokens);
+        users[_Id] = User(_Id, _name,_tokens,  new uint256[](0));
         emit UserAdded(msg.sender, _name);
+        return true;
     }
 
-    function checkOut(uint256 _bookId) external {
+    function BookIssue(uint256 _bookId) external {
         require(_bookId <= totalBooks && _bookId > 0, "Invalid book ID");
         require(!books[_bookId].checkedOut, "Book already checked out");
         require(users[msg.sender].id != address(0), "User does not exist");
-
+        address _Id = users[msg.sender].id;
+        uint256 bookFee = books[_bookId].price;
+         IERC20(token).transferFrom(_Id, owner, bookFee);
         books[_bookId].borrower = msg.sender;
         books[_bookId].checkedOut = true;
+        users[msg.sender].balance-= bookFee;
         users[msg.sender].borrowedBooks.push(_bookId);
 
         emit BookCheckedOut(_bookId, msg.sender);
--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -4,8 +4,13 @@ async function main(){
     const signer = await hre.ethers.provider.getSigner();
     console.log("signer : ", signer.address);
 
-    const contract = await hre.ethers.deployContract("Library");
+    const token = await hre.ethers.deployContract("LibraryToken", [signer.address]);
+    await token.waitForDeployment();
+
+    const contract = await hre.ethers.deployContract("Library", [token.target]);
     await contract.waitForDeployment();
+
+    console.log("token address : ", token.target);
 
     console.log("deployed at :", contract.target);
 }
